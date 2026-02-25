e800d87cf1bffc05f5cff6a563c011ac4dd29ca9
AbhisumatK <kunduabhisumat@gmail.com>
2026-02-25 20:19:13 +0530

    Chaged the LLM model, fixed critical bugs

--- preprocess.py
+++ preprocess.py
@@ -5,16 +5,18 @@ from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.exceptions import OutputParserException
 
 
-def process_posts(raw_file_path, processed_file_path=None):
+def process_posts(raw_file_path, processed_file_path="data/processed_posts.json"):
+    enriched_posts = []
     with open('posts.json', encoding='utf-8') as file:
         posts = json.load(file)
-        enriched_posts = []
+        
         for post in posts:
             metadata = extract_metadata(post['text'])
             post_with_metadata = post | metadata
             enriched_posts.append(post_with_metadata)
 
     unified_tags = get_unified_tags(enriched_posts)
+    
     for post in enriched_posts:
         current_tags = post['tags']
         new_tags = {unified_tags[tag] for tag in current_tags}
@@ -26,11 +28,10 @@ def process_posts(raw_file_path, processed_file_path=None):
 
 def extract_metadata(post):
     template = '''
-    You are given a LinkedIn post. You need to extract number of lines, language of the post and tags.
+    You are given a LinkedIn post. You need to extract number of lines and tags.
     1. Return a valid JSON. No preamble. 
-    2. JSON object should have exactly three keys: line_count, language and tags. 
+    2. JSON object should have exactly 2 keys: line_count and tags. 
     3. tags is an array of text tags. Extract maximum two tags.
-    4. Language should be English
     
     Here is the actual post on which you need to perform this task:  
     {post}
